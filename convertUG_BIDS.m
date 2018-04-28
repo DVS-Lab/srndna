067e614cf7cf33774370c17f31b8583f8b705b6d
David V. Smith <david.victor.smith@gmail.com>
2018-04-28 09:56:26 -0400

    improved output
    differentiated blocks and events. flagged missed trials

--- convertUG_BIDS.m
+++ convertUG_BIDS.m
@@ -9,6 +9,7 @@ fclose(fid);
 % "Feedback" is the offer value (out of $20)
 
 onset = C{9};
+iti = C{6}; % if > 1, it's the beginning of a new block
 RT = C{12} - C{9};
 duration = C{14};
 %trialtype --> C{2} is IsFairBlock and C{5} is Partner
@@ -26,6 +27,7 @@ fname = sprintf('sub-%03d_task-%s_events.tsv',subnum,task);
 fid = fopen(fullfile(maindir,fname),'w');
 fprintf(fid,'onset\tduration\ttrial_type\tresponse_time\tPartnerKeeps\tOffer\tResponse\n');
 for t = 1:length(onset);
+    
     %fprintf(fid,'onset\tduration\ttrial_type\tresponse_time\tPartnerKeeps\tOffer\tResponse\n');
     if (IsFairBlock(t) == 1) && (Partner(t) == 1)
         trial_type = 'computer_fair';
@@ -41,7 +43,14 @@ for t = 1:length(onset);
         trial_type = 'friend_unfair';
     end
     
-    fprintf(fid,'%f\t%f\t%s\t%f\t%d\t%d\t%d\n',onset(t),duration(t),trial_type,RT(t),PartnerKeeps(t),Offer(t),response(t));
+    if ~isnan(Offer(t))
+        fprintf(fid,'%f\t%f\t%s\t%f\t%d\t%d\t%d\n',onset(t),duration(t),['event_' trial_type],RT(t),PartnerKeeps(t),Offer(t),response(t));
+    else
+        fprintf(fid,'%f\t%f\t%s\t%f\t%d\t%d\t%d\n',onset(t),duration(t),'missed_trial',3.5,PartnerKeeps(t),999,999);
+    end
+    if iti(t) > 1
+        fprintf(fid,'%f\t%f\t%s\t%f\t%d\t%d\t%d\n',onset(t),31,['block_' trial_type],999,999,999,999);
+    end
     
 end
 fclose(fid);
