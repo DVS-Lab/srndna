1ca339910dd9f0fe0075cee08fb1ce95a76aaa35
David V. Smith <david.victor.smith@gmail.com>
2018-07-30 00:13:59 -0400

    shortened ISIs and ITIs

--- genTrustDesign.m
+++ genTrustDesign.m
@@ -15,18 +15,18 @@ maindir = pwd;
 outfiles = fullfile(maindir,'psychopy','params','TG_designs');
 mkdir(outfiles);
 
-for s = 899
-    ss = s + 100;
-    subout = fullfile(outfiles,sprintf('sub-%03d',ss));
+subs = [101:299 999];
+for s = subs
+    subout = fullfile(outfiles,sprintf('sub-%03d',s));
     mkdir(subout);
     for r = 1:6
         
         ntrials = 36;
         choice_dur = 3;
-        ISI_list = [repmat(2,1,18) repmat(4,1,10) repmat(6,1,6) repmat(8,1,2)];
+        ISI_list = [repmat(2,1,20) repmat(3.5,1,10) repmat(5,1,6)];
         ISI_list = ISI_list(randperm(length(ISI_list)));
         ISI_list = ISI_list - 1.5; %shaving 1.5 seconds to account for "WAITING" screen
-        ITI_list = [repmat(2,1,18) repmat(4,1,10) repmat(6,1,5) repmat(8,1,2) 10];
+        ITI_list = [repmat(2,1,18) repmat(4,1,10) repmat(6,1,5) repmat(8,1,3)];
         ITI_list = ITI_list(randperm(length(ITI_list)));
         
         choice_pairs = combnk([0 2 4 8],2);
@@ -37,7 +37,7 @@ for s = 899
             choice_pairs ones(6,1)*1 ones(6,1);
             choice_pairs ones(6,1)*1 zeros(6,1)];
         
-        fname = fullfile(subout,sprintf('sub-%03d_run-%02d_design.csv',ss,r));
+        fname = fullfile(subout,sprintf('sub-%03d_run-%02d_design.csv',s,r));
         
         fid = fopen(fname,'w');
         fprintf(fid,'Trial,cLeft,cRight,Partner,Reciprocate,ISI,ITI\n');
