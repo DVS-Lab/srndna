b2eb0ce9e110f8a49a9152f098e8f1a8e0393f5c
DVSneuro <david.v.smith@temple.edu>
2018-08-03 11:27:15 -0400

    fixed error in protocol_name

--- heuristics.py
+++ heuristics.py
@@ -32,7 +32,7 @@ def infotodict(seqinfo):
             info[t2w] = [s.series_id]
         if (s.dim4 == 218) and ('trust' in s.protocol_name):
             info[trust].append({'item': s.series_id})
-        if (s.dim4 == 202) and ('reward' in s.series_id):
+        if (s.dim4 == 202) and ('reward' in s.protocol_name):
             info[sharedreward].append({'item': s.series_id})
         if (s.dim4 == 197) and ('ultimatum' in s.protocol_name):
             info[ultimatum].append({'item': s.series_id})
