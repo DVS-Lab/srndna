44a6ac5c62363485817502f514cf69b675cfef5c
David V. Smith <david.victor.smith@gmail.com>
2018-08-05 10:29:16 -0400

    renamed and removed

--- batch_convert.sh
+++
@@ -1,7 +0,0 @@
-#!/#!/usr/bin/env bash
-
-sudo docker run --rm -it -v /data/projects/srndna:/data:ro \
--v /data/projects/srndna/bids:/output \
-nipy/heudiconv:latest \
--d /data/dicoms/SMITH-AgingDM-{subject}/scans/*/DICOM/*.dcm -s 102 \
--f /data/heuristics.py -c dcm2niix -b -o /output
--- mriqc_example.sh
+++
@@ -1,9 +0,0 @@
-# example code for MRIQC
-# runs MRIQC on all subjects in BIDS_out_all
-
-sudo docker run -it --rm \
--v /data/projects/srndna/bids:/data:ro \
--v /data/projects/srndna/mriqc:/out \
-poldracklab/mriqc:latest \
-/data /out \
-participant
