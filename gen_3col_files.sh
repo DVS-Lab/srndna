a20d72382c1b2516f4b2fcecd7d3034682e5cf9b
David V. Smith <david.victor.smith@gmail.com>
2018-08-05 10:28:04 -0400

    take  input

--- gen_3col_files.sh
+++ gen_3col_files.sh
@@ -6,29 +6,35 @@ if [ ! -d ${baseout} ]; then
   mkdir -p $baseout
 fi
 
-for sub in 102; do
-  for run in 1 2 3 4 5; do
-    input=${maindir}/bids/sub-${sub}/func/sub-${sub}_task-trust_run-0${run}_events.tsv
-    output=${baseout}/sub-${sub}/trust
-    mkdir -p $output
-    if [ -e $input ]; then
-      bash BIDSto3col.sh $input ${output}/run-0${run}
-    fi
-  done
-  for run in 1 2; do
-    input=${maindir}/bids/sub-${sub}/func/sub-${sub}_task-sharedreward_run-0${run}_events.tsv
-    output=${baseout}/sub-${sub}/sharedreward
-    mkdir -p $output
-    if [ -e $input ]; then
-      bash BIDSto3col.sh $input ${output}/run-0${run}
-    fi
-  done
-  for run in 1 2; do
-    input=${maindir}/bids/sub-${sub}/func/sub-${sub}_task-ultimatum_run-0${run}_events.tsv
-    output=${baseout}/sub-${sub}/ultimatum
-    mkdir -p $output
-    if [ -e $input ]; then
-      bash BIDSto3col.sh $input ${output}/run-0${run}
-    fi
-  done
+sub=$1
+
+for run in 1 2 3 4 5; do
+  input=${maindir}/bids/sub-${sub}/func/sub-${sub}_task-trust_run-0${run}_events.tsv
+  output=${baseout}/sub-${sub}/trust
+  mkdir -p $output
+  if [ -e $input ]; then
+    bash BIDSto3col.sh $input ${output}/run-0${run}
+  else
+    echo "PATH ERROR: cannot locate ${input}."
+  fi
+done
+for run in 1 2; do
+  input=${maindir}/bids/sub-${sub}/func/sub-${sub}_task-sharedreward_run-0${run}_events.tsv
+  output=${baseout}/sub-${sub}/sharedreward
+  mkdir -p $output
+  if [ -e $input ]; then
+    bash BIDSto3col.sh $input ${output}/run-0${run}
+  else
+    echo "PATH ERROR: cannot locate ${input}."
+  fi
+done
+for run in 1 2; do
+  input=${maindir}/bids/sub-${sub}/func/sub-${sub}_task-ultimatum_run-0${run}_events.tsv
+  output=${baseout}/sub-${sub}/ultimatum
+  mkdir -p $output
+  if [ -e $input ]; then
+    bash BIDSto3col.sh $input ${output}/run-0${run}
+  else
+    echo "PATH ERROR: cannot locate ${input}."
+  fi
 done
