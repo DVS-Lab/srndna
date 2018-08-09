9b9a19fb3f62cae29aba261b72e78a449b4ed864
DVSneuro <david.v.smith@temple.edu>
2018-08-09 17:20:07 -0400

    make fieldmaps compatible with BIDS, h/t Jeff

--- run_heudiconv.sh
+++ run_heudiconv.sh
@@ -7,16 +7,17 @@
 
 sub=$1
 xnat=$2
+bidsroot=/data/projects/srndna/bids
 
 if [ $xnat == 1 ]; then
   sudo docker run --rm -it -v /data/projects/srndna:/data:ro \
-  -v /data/projects/srndna/bids:/output \
+  -v ${bidsroot}:/output \
   nipy/heudiconv:latest \
   -d /data/dicoms/SMITH-AgingDM-{subject}/scans/*/DICOM/*.dcm -s $sub \
   -f /data/heuristics.py -c dcm2niix -b -o /output
 else
   sudo docker run --rm -it -v /data/projects/srndna:/data:ro \
-  -v /data/projects/srndna/bids:/output \
+  -v ${bidsroot}:/output \
   nipy/heudiconv:latest \
   -d /data/dicoms/SMITH-AgingDM-{subject}/*/*/*.IMA -s $sub \
   -f /data/heuristics.py -c dcm2niix -b -o /output
@@ -24,3 +25,23 @@ fi
 
 # need to select a version and stick with it instead of nipy/heudiconv:latest
 # need to check anonymization (e.g., dcm2niix -ba -y)???
+
+
+# FMAP_INTENDEDFOR  set the list of func filenames correctly here (relative paths starting from within sub-### folder)
+# will have to adjust for subjects who don't have this. need a better way to do this
+FUNC01=\"sub-${sub}_task-sharedreward_run-01_bold.nii.gz\"
+FUNC02=\"sub-${sub}_task-sharedreward_run-02_bold.nii.gz\"
+FUNC03=\"sub-${sub}_task-trust_run-01_bold.nii.gz\"
+FUNC04=\"sub-${sub}_task-trust_run-02_bold.nii.gz\"
+FUNC05=\"sub-${sub}_task-trust_run-03_bold.nii.gz\"
+FUNC06=\"sub-${sub}_task-trust_run-04_bold.nii.gz\"
+FUNC07=\"sub-${sub}_task-trust_run-05_bold.nii.gz\"
+FUNC08=\"sub-${sub}_task-ultimatum_run-01_bold.nii.gz\"
+FUNC09=\"sub-${sub}_task-ultimatum_run-02_bold.nii.gz\"
+
+
+#FMAP_INTENDEDFOR edit the line below so that it only includes as many FUNC_FILENAME as you need
+#the formatting of this line is kind of tricky with all the special characters: \n \t
+#be sure to include ,\n\t\t in between each ${FUNCFILENAME} (but not after the last one)
+#after the last ${FUNCFILENAME} in the line there should be the characters ],/g before the closing quotation mark (")
+sed -i "1s/{/{\n\t\"IntendedFor\": [${FUNC01},\n\t\t${FUNC02},\n\t\t${FUNC03}],\n\t\t${FUNC04}],\n\t\t${FUNC05}],\n\t\t${FUNC06}],\n\t\t${FUNC07}],\n\t\t${FUNC08}],\n\t\t${FUNC09}],/g" ${bidsroot}/sub-${sub}/fmap/sub-${sub}_*.json
