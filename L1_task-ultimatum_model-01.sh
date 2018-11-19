5310e7bb3a4c4e12738d98719d2808f8c36f8bc9
DVSneuro <david.v.smith@temple.edu>
2018-11-19 00:38:47 -0500

    add ppi functionality

--- L1_task-ultimatum_model-01.sh
+++ L1_task-ultimatum_model-01.sh
@@ -2,26 +2,54 @@
 
 maindir=`pwd`
 
+TASK=ultimatum
+
 sub=$1
 run=$2
+ppi=$3 # 0 for activation, otherwise name of the roi
 
-EVDIR=${maindir}/fsl/EVfiles/sub-${sub}/ultimatum/run-0${run}
-DATA=${maindir}/fmriprep/fmriprep/sub-${sub}/func/sub-${sub}_task-ultimatum_run-0${run}_bold_space-MNI152NLin2009cAsym_variant-smoothAROMAnonaggr_preproc.nii.gz
-MAINOUTPUT=${maindir}/fsl/sub-${sub}/
+MAINOUTPUT=${maindir}/fsl/sub-${sub}
 mkdir -p $MAINOUTPUT
-OUTPUT=${MAINOUTPUT}/L1_task-ultimatum_model-01_type-act_run-0${run}
+
+EVDIR=${maindir}/fsl/EVfiles/sub-${sub}/${TASK}/run-0${run}
+if [ $ppi -eq 0 ]; then
+	DATA=${maindir}/fmriprep/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_run-0${run}_bold_space-MNI152NLin2009cAsym_variant-smoothAROMAnonaggr_preproc.nii.gz
+	TYPE=act
+	OUTPUT=${MAINOUTPUT}/L1_task-${TASK}_model-01_type-${TYPE}_run-0${run}
+else
+	DATA=${MAINOUTPUT}/L1_task-${TASK}_model-01_type-act_run-0${run}.feat/filtered_func_data.nii.gz
+	TYPE=ppi
+	OUTPUT=${MAINOUTPUT}/L1_task-${TASK}_model-01_type-${TYPE}_seed-${ppi}_run-0${run}
+fi
+
 if [ -e ${OUTPUT}.feat/cluster_mask_zstat1.nii.gz ]; then
 	exit
 else
 	rm -rf ${OUTPUT}.feat
 fi
 
-ITEMPLATE=${maindir}/templates/L1_task-ultimatum_model-01_type-act.fsf
-OTEMPLATE=${MAINOUTPUT}/L1_task-ultimatum_model-01_type-act_run-0${run}.fsf
-sed -e 's@OUTPUT@'$OUTPUT'@g' \
--e 's@DATA@'$DATA'@g' \
--e 's@EVDIR@'$EVDIR'@g' \
-<$ITEMPLATE> $OTEMPLATE
+ITEMPLATE=${maindir}/templates/L1_task-${TASK}_model-01_type-${TYPE}.fsf
+OTEMPLATE=${MAINOUTPUT}/L1_task-${TASK}_model-01_type-${TYPE}_run-0${run}.fsf
+
+if [ $ppi -eq 0 ]; then
+	sed -e 's@OUTPUT@'$OUTPUT'@g' \
+	-e 's@DATA@'$DATA'@g' \
+	-e 's@EVDIR@'$EVDIR'@g' \
+	-e 's@MISSED_TRIAL@'$MISSED_TRIAL'@g' \
+	-e 's@EV_SHAPE@'$EV_SHAPE'@g' \
+	<$ITEMPLATE> $OTEMPLATE
+else
+	PHYS=${MAINOUTPUT}/ts_task-trust_mask-${ppi}_run-0${run}.txt
+	MASK=${maindir}/masks/mask-${ppi}_task-trust.nii.gz
+	fslmeants -i $DATA -o $PHYS -m $MASK
+	sed -e 's@OUTPUT@'$OUTPUT'@g' \
+	-e 's@DATA@'$DATA'@g' \
+	-e 's@EVDIR@'$EVDIR'@g' \
+	-e 's@MISSED_TRIAL@'$MISSED_TRIAL'@g' \
+	-e 's@EV_SHAPE@'$EV_SHAPE'@g' \
+	-e 's@PHYS@'$PHYS'@g' \
+	<$ITEMPLATE> $OTEMPLATE
+fi
 
 # runs feat on output template
 feat $OTEMPLATE
@@ -34,7 +62,10 @@ ln -s $FSLDIR/etc/flirtsch/ident.mat ${OUTPUT}.feat/reg/standard2example_func.ma
 ln -s ${OUTPUT}.feat/mean_func.nii.gz ${OUTPUT}.feat/reg/standard.nii.gz
 
 # delete unused files
-# not deleting filtered_func_data bc that's input for PPI
 rm -rf ${OUTPUT}.feat/stats/res4d.nii.gz
 rm -rf ${OUTPUT}.feat/stats/corrections.nii.gz
 rm -rf ${OUTPUT}.feat/stats/threshac1.nii.gz
+
+if [ ! $ppi -eq 0 ]; then
+	rm -rf ${OUTPUT}.feat/filtered_func_data.nii.gz
+fi
--- runall_L1stats.sh
+++ runall_L1stats.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
 
-
-for subrun in "111 5" "115 5" "116 5" "117 5" "118 5" "120 5" "121 5"; do
-
-  set -- $subrun
-  sub=$1
-  nruns=$2
-  #echo "$sub $nruns"
-
-  for run in `seq $nruns`; do
-  	#Manages the number of jobs and cores
-  	SCRIPTNAME=L1_task-trust_model-01.sh
-  	NCORES=16
-  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
-    		sleep 1s
-  	done
-  	bash $SCRIPTNAME $sub $run &
-  	sleep 5s
-  done
-
-  for run in `seq 2`; do
-  	#Manages the number of jobs and cores
-  	SCRIPTNAME=L1_task-sharedreward_model-01.sh
-  	NCORES=5
-  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
-    		sleep 1s
-  	done
-  	bash $SCRIPTNAME $sub $run &
-  	sleep 5s
-  done
-
-  for run in `seq 2`; do
-  	#Manages the number of jobs and cores
-  	SCRIPTNAME=L1_task-ultimatum_model-01.sh
-  	NCORES=5
-  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
-    		sleep 1s
-  	done
-  	bash $SCRIPTNAME $sub $run &
-  	sleep 5s
-  done
-
-
+for ppi in "ffa" "vs" "amyg"; do
+	
+	for subrun in "111 5" "115 5" "116 5" "117 5" "118 5" "120 5" "121 5"; do
+	
+	  set -- $subrun
+	  sub=$1
+	  nruns=$2
+	  #echo "$sub $nruns"
+	
+	  for run in `seq $nruns`; do
+	  	#Manages the number of jobs and cores
+	  	SCRIPTNAME=L1_task-trust_model-01.sh
+	  	NCORES=16
+	  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+	    		sleep 1s
+	  	done
+	  	bash $SCRIPTNAME $sub $run $ppi &
+	  	sleep 5s
+	  done
+	
+	  for run in `seq 2`; do
+	  	#Manages the number of jobs and cores
+	  	SCRIPTNAME=L1_task-sharedreward_model-01.sh
+	  	NCORES=5
+	  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+	    		sleep 1s
+	  	done
+	  	bash $SCRIPTNAME $sub $run $ppi &
+	  	sleep 5s
+	  done
+	
+	  for run in `seq 2`; do
+	  	#Manages the number of jobs and cores
+	  	SCRIPTNAME=L1_task-ultimatum_model-01.sh
+	  	NCORES=5
+	  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+	    		sleep 1s
+	  	done
+	  	bash $SCRIPTNAME $sub $run $ppi &
+	  	sleep 5s
+	  done
+	
+	
+	done
 done
