860c74804af048d2962e08830b462c996c89a144
DVSneuro <david.v.smith@temple.edu>
2018-11-20 12:17:32 -0500

    add ppi functionality

--- L3_task-all_model-01.sh
+++ L3_task-all_model-01.sh
@@ -5,22 +5,23 @@ maindir=`pwd`
 task=$1
 copenum=$2
 copename=$3
+other=$4 # type-ppi_seed-ffa
 
-MAINOUTPUT=${maindir}/fsl/L3_all_n16_fixed
+MAINOUTPUT=${maindir}/fsl/L3_model-01_ppi_n16_rfx
 mkdir -p $MAINOUTPUT
+REPLACEME=task-${task}_model-01_${other}
 
-
-OUTPUT=${MAINOUTPUT}/L3_task-${task}_model-01_type-act_copenum-${copenum}_copename-${copename}
-if [ -e ${OUTPUT}.gfeat/cope${NCOPES}.feat/cluster_mask_zstat1.nii.gz ]; then
+OUTPUT=${MAINOUTPUT}/L3_task-${task}_${other}_copename-${copename}
+if [ -e ${OUTPUT}.gfeat/cope1.feat/cluster_mask_zstat1.nii.gz ]; then
 	echo "skipping existing output"
 else
 	rm -rf ${OUTPUT}.gfeat
 
 	ITEMPLATE=${maindir}/templates/L3_template_n16.fsf
-	OTEMPLATE=${MAINOUTPUT}/L3_task-${task}_model-01_type-act_copenum-${copenum}.fsf
+	OTEMPLATE=${MAINOUTPUT}/L3_task-${task}_${REPLACEME}_copenum-${copenum}.fsf
 	sed -e 's@OUTPUT@'$OUTPUT'@g' \
 	-e 's@COPENUM@'$copenum'@g' \
-	-e 's@TASK@'$task'@g' \
+	-e 's@REPLACEME@'$REPLACEME'@g' \
 	<$ITEMPLATE> $OTEMPLATE
 	feat $OTEMPLATE
 
