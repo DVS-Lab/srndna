74c0f70b2ffdc39c57769d492474016e349f2fda
dfareri <dominic.fareri@gmail.com>
2018-12-17 09:36:42 -0500

    UG Behavioral Analysis script

--- analyzeUG.m
+++ analyzeUG.m
@@ -2,17 +2,18 @@ clear;
 maindir = pwd;
 
 % open output files
-fname = fullfile(maindir,'UG_summary_run_12_2018_InOut.csv');
+fname = fullfile(maindir,'UG_summary_run_12_2018_InOut_FairUnfair.csv');
 fid_run = fopen(fname,'w');
-fprintf(fid_run,'subnum,run,avg_comp,avg_OutGrp,avg_InGrp,avg_comp_RT,avg_OutGrp_RT,avg_InGrp_RT,misses\n');
-fname = fullfile(maindir,'UG_summary_subj_12_2018InOut.csv');
+%fprintf(fid_run,'subnum,run,avg_comp,avg_OutGrp,avg_InGrp,avg_comp_RT,avg_OutGrp_RT,avg_InGrp_RT,misses\n');
+fprintf(fid_run,'subnum,run,avg_compFair,avg_compUnfair,avg_OutGrpFair,avg_OutGrpUnfair,avg_InGrpFair,avg_InGrpUnfair,avg_compFairRT,avg_compUnfairRT,avg_OutGrpFairRT,avg_OutGrpUnfairRT,avg_InGrpFairRT,avg_InGrpUnfairRT,misses\n');
+fname = fullfile(maindir,'UG_summary_subj_12_2018InOut_FairUnfair.csv');
 fid_subj = fopen(fname,'w');
-fprintf(fid_subj,'subnum,avg_comp,avg_OutGrp,avg_InGrp,avg_comp_RT,avg_OutGrp_RT,avg_InGrp_RT,misses\n');
-
-%sublist = [103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 120 121];
+%fprintf(fid_subj,'subnum,avg_comp,avg_OutGrp,avg_InGrp,avg_comp_RT,avg_OutGrp_RT,avg_InGrp_RT,misses\n');
+fprintf(fid_subj,'subnum,avg_compFair,avg_compUnfair,avg_OutGrpFair,avg_OutGrpUnfair,avg_InGrpFair,avg_InGrpUnfair,avg_compFairRT,avg_compUnfairRT,avg_OutGrpFairRT,avg_OutGrpUnfairRT,avg_InGrpFairRT,avg_InGrpUnfairRT,misses\n');
 
 %scan subjects only
 sublist = [104 105 106 107 108 109 110 111 112 113 115 116 117 118 120 121 122 124 125 126];
+
 %ethinicity=0: 105-111, 115, 117, 120-122
 %ethnicity=1: 104, 112-114, 116, 118-119, 124-126
 
@@ -22,56 +23,59 @@ for s = 1:length(sublist)
     
     subj = sublist(s);
     runs = 2; 
-    %{
-    if subj == 106 || subj == 213
-        runs = 3;
-    elseif subj == 109 || subj == 110
-        runs = 2;
-    elseif subj == 207 || subj == 210 || subj == 211 || subj == 212 || subj == 215 || subj == 216 || subj == 217 || subj == 221
-        runs = 4;
-    else
-        runs = 5;
-    end
-    %}
     
-    tmp_data = zeros(runs,7);
+    tmp_data = zeros(runs,13);
     for r = 1:runs
-        fname = fullfile(maindir,'psychopy','logs',num2str(subj),sprintf('sub-%03d_task-ultimatum_run-%d_raw.csv',subj,r-1));
+        fname = fullfile(maindir,'psychopy','logs',num2str(subj),sprintf('sub-%03d_task-ultimatum_run-%d_raw.csv',subj,r-1)); % creates variable to store the path to a given subject's data
         fid = fopen(fname,'r');
-        C = textscan(fid,[repmat('%f',1,14) '%s' repmat('%f',1,9)],'Delimiter',',','HeaderLines',1,'EmptyValue', NaN);
+        C = textscan(fid,[repmat('%f',1,14) '%s' repmat('%f',1,9)],'Delimiter',',','HeaderLines',1,'EmptyValue', NaN); %creates a cell array C reading in the .csv file with header rows
         fclose(fid);
         
-        RT = C{12}; % will do something with this later
-        % partner is 1 (computer), 2 (outgroup), 3 (ingroup)
-        Partner = C{5};
-        response = C{11}; % 2 = reject, 3 = accept (with '999' for no response)
-        offer = C{4}; %1-10
-        data = [Partner offer response RT];
+        RT = C{12}; % create variable to store reaction time 
+        Partner = C{5}; % create variable to store partner: 1 (computer), 2 (outgroup), 3 (ingroup)
+        response = C{11}; % create variable to store response: 2 = reject, 3 = accept (with '999' for no response)
+        offer = C{4}; % create variable to store offer amount: 1-10
+        block = C{2}; % create variable to store block type: 1 = fair, 0 = unfair
+        data = [Partner block offer response RT]; % creates matrix to hold subject data of relevance
         
-        misses = length(find(data(:,3)==999));
-        data(data(:,3)==999,:) = [];
-        data(data(:,3)==2,3) = 0;
-        data(data(:,3)==3,3) = 1;
+        misses = length(find(data(:,3)==999)); %create vector of missed trials
+        data(data(:,4)==999,:) = []; % remove rows of missed trials from data for calculations
+        data(data(:,4)==2,4) = 0; % recode reject from 2 to 0
+        data(data(:,4)==3,4) = 1; % recode accept from 3 to 1
         Partner = data(:,1);
-        offer = data(:,2);
-        response = data(:,3);
-        logRT = log(RT);
-        logRT = data(:,4);
+        block = data(:,2);
+        offer = data(:,3);
+        response = data(:,4);
+        logRT = log(RT); % log transform RT
+        logRT = data(:,5);
         
-        avg_comp = mean(response(Partner==1,1));
-        avg_outgroup = mean(response(Partner==2,1));
-        avg_ingroup = mean(response(Partner==3,1));
-        avg_comp_logRT = mean(logRT(Partner==1,1));
-        avg_outgroup_logRT = mean(logRT(Partner==2,1));
-        avg_ingroup_logRT = mean(logRT(Partner==3,1));
-        tmp_data(r,:) = [avg_comp avg_outgroup avg_ingroup avg_comp_logRT avg_outgroup_logRT avg_ingroup_logRT misses];
+        % compute average proportion of accepted trials per partner
+        avg_compFair = mean(response(Partner==1 & block == 1,1));
+        avg_compUnfair = mean(response(Partner==1 & block == 0,1));
+        avg_outgroupFair = mean(response(Partner==2 & block == 1,1));
+        avg_outgroupUnfair = mean(response(Partner==2 & block == 0,1));
+        avg_ingroupFair = mean(response(Partner==3 & block == 1,1));
+        avg_ingroupUnfair = mean(response(Partner==3 & block == 0,1));
         
+        %compute average logRT per partner
+        avg_compFair_logRT = mean(logRT(Partner==1 & block == 1,1));
+        avg_compUnfair_logRT = mean(logRT(Partner==1 & block == 0,1));
+        avg_outgroupFair_logRT = mean(logRT(Partner==2 & block == 1,1));
+        avg_outgroupUnfair_logRT = mean(logRT(Partner==2 & block == 0,1));
+        avg_ingroupFair_logRT = mean(logRT(Partner==3 & block == 1,1));
+        avg_ingroupUnfair_logRT = mean(logRT(Partner==3 & block == 0,1));
+        
+        %fill temporary matrix with data to be written out
+        tmp_data(r,:) = [avg_compFair avg_compUnfair avg_outgroupFair avg_outgroupUnfair avg_ingroupFair avg_ingroupUnfair avg_compFair_logRT avg_compUnfair_logRT avg_outgroupFair_logRT avg_outgroupUnfair_logRT avg_ingroupFair_logRT avg_ingroupUnfair_logRT misses];
+        
+        %write out averages per run
         % subnum,run,avg_comp,avg_stranger,avg_friend,pct_miss
-        fprintf(fid_run,'%d,%d,%f,%f,%f,%f,%f,%f,%f\n',subj,r,tmp_data(r,:));
+        fprintf(fid_run,'%d,%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n',subj,r,tmp_data(r,:));
         
     end
+    % write out average across runs
     % subnum,avg_comp,avg_stranger,avg_friend,pct_miss
-    fprintf(fid_subj,'%d,%f,%f,%f,%f,%f,%f,%f\n',subj,mean(tmp_data));
+    fprintf(fid_subj,'%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n',subj,mean(tmp_data));
     
 end
 fclose(fid_subj);
