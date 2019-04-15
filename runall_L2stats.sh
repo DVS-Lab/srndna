f6ab5ba13ba1c7f1d05d154ee074694ff3acbd5c
DVSneuro <david.v.smith@temple.edu>
2019-04-14 23:41:26 -0400

    update loops and timing

--- runall_L2stats.sh
+++ runall_L2stats.sh
@@ -2,17 +2,18 @@
 
 for sm in 6; do
 		
-		for subrun in "104 5" "105 5" "106 3" "107 5" "108 5" "109 2" "110 2" "112 5" "113 5" "111 5" "115 5" "116 5" "117 5" "118 5" "120 5" "121 5" "122 5" "124 5" "125 5" "126 5" "127 5" "128 5" "129 5" "130 5" "131 5" "132 5" "133 5" "134 4"; do
+	for subrun in "104 5" "105 5" "106 3" "107 5" "108 5" "109 2" "110 2" "112 5" "113 5" "111 5" "115 5" "116 5" "117 5" "118 5" "120 5" "121 5" "122 5" "124 5" "125 5" "126 5" "127 5" "128 5" "129 5" "130 5" "131 5" "132 5" "133 5" "134 4"; do
 		set -- $subrun
 		sub=$1
 		nruns=$2
 		bash L2_task-all_model-01_type-act.sh $sub $nruns $sm &
-		sleep 5
+		sleep 10s
 
 	done
 
 done
 
+sleep 5m
 
 for sm in 6; do
 	for ppi in "VS" "FFA" "Amyg"; do
@@ -21,21 +22,22 @@ for sm in 6; do
 			sub=$1
 			nruns=$2
 			bash L2_task-all_model-01_type-ppi.sh $sub $nruns $ppi $sm &
-			sleep 5
+			sleep 10s
 	
 		done
 	done
 done
 
+sleep 5m
 
-#for ppi in "dmn" "ecn"; do
-#		for subrun in "104 5" "105 5" "106 3" "107 5" "108 5" "109 2" "110 2" "112 5" "113 5" "111 5" "115 5" "116 5" "117 5" "118 5" "120 5" "121 5" "122 5" "124 5" "125 5" "126 5" "127 5" "128 5" "129 5" "130 5" "131 5" "132 5" "133 5" "134 4"; do
-#	
-#		set -- $subrun
-#		sub=$1
-#		nruns=$2
-#		bash L2_task-all_model-01_type-nppi.sh $sub $nruns $ppi &
-#		sleep 10
-#
-#	done
-#done
+for ppi in "dmn" "ecn"; do
+	for subrun in "104 5" "105 5" "106 3" "107 5" "108 5" "109 2" "110 2" "112 5" "113 5" "111 5" "115 5" "116 5" "117 5" "118 5" "120 5" "121 5" "122 5" "124 5" "125 5" "126 5" "127 5" "128 5" "129 5" "130 5" "131 5" "132 5" "133 5" "134 4"; do
+	
+		set -- $subrun
+		sub=$1
+		nruns=$2
+		bash L2_task-all_model-01_type-nppi.sh $sub $nruns $ppi &
+		sleep 10s
+
+	done
+done
