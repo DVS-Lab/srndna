9386f7c2568bf064e29503fc7be94089a297b9da
DVSneuro <david.v.smith@temple.edu>
2019-04-17 13:03:10 -0400

    run for n=32 and fix errors

--- runall_L3stats.sh
+++ runall_L3stats.sh
@@ -6,7 +6,7 @@
 for other in "type-nppi-dmn_sm-6" "type-nppi-ecn_sm-6" "type-ppi_seed-FFA_sm-6" "type-ppi_seed-Amyg_sm-6" "type-ppi_seed-VS_sm-6" "type-act_sm-6"; do
 	for subrun in "7 unfair-fair" "8 soc-comp" "9 fairness_IG-OG" "10 fairness_IG-C" "11 fairness_OG-C" "12 phys"; do
 	
-		if [ "${other}" == "type-act_sm-6" ] && [ "${subnum}" == "12 phys" ]; then
+		if [ "${other}" == "type-act_sm-6" ] && [ "${subrun}" == "12 phys" ]; then
 			echo "skipping phys for activation since it does not exist..."
 			continue
 		fi
@@ -23,7 +23,7 @@ for other in "type-ppi_seed-FFA_sm-6" "type-ppi_seed-Amyg_sm-6" "type-ppi_seed-V
 	
 	for subrun in "7 rew-pun" "8 F-S" "9 F-C" "10 FS-C" "11 reward_F-S" "12 reward_S-C" "13 reward_F-C" "14 phys"; do
 		
-		if [ "${other}" == "type-act_sm-6" ] && [ "${subnum}" == "14 phys" ]; then
+		if [ "${other}" == "type-act_sm-6" ] && [ "${subrun}" == "14 phys" ]; then
 			echo "skipping phys for activation since it does not exist..."
 			continue
 		fi
@@ -37,7 +37,7 @@ for other in "type-ppi_seed-FFA_sm-6" "type-ppi_seed-Amyg_sm-6" "type-ppi_seed-V
 	
 	for subrun in "10 rec-def" "11 face" "12 reciprocation_F-S" "13 F-S" "14 F-C" "15 S-C" "16 rec_SocClose" "17 def_SocClose" "18 rec-def_SocClose" "19 phys"; do
 
-		if [ "${other}" == "type-act_sm-6" ] && [ "${subnum}" == "19 phys" ]; then
+		if [ "${other}" == "type-act_sm-6" ] && [ "${subrun}" == "19 phys" ]; then
 			echo "skipping phys for activation since it does not exist..."
 			continue
 		fi		
