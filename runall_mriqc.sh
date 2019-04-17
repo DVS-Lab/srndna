c76e5d865f5339610fe167e0a6417eae4fa04c25
DVSneuro <david.v.smith@temple.edu>
2019-04-17 13:02:32 -0400

    run for everyone and use all processors

--- runall_mriqc.sh
+++ runall_mriqc.sh
@@ -3,6 +3,8 @@
 # usage: bash run_mriqc.sh sub
 # example: bash run_mriqc.sh 102
 
+#echo "sleeping for 30 minutes at `date`"
+#sleep 30m 
 
 docker run -it --rm \
 -v /data/projects/srndna/bids:/data:ro \
@@ -10,8 +12,8 @@ docker run -it --rm \
 -v /data/projects/srndna/scratch:/scratch \
 -u $(id -u):$(id -g) \
 -w /scratch \
-poldracklab/mriqc:0.12.1 \
+poldracklab/mriqc:0.15.0 \
 /data /out \
-participant --n_cpus 12 --fft-spikes-detector --ica -w /scratch
+participant --fft-spikes-detector --ica -w /scratch
 
 # #--participant_label $sub
