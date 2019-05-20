88f5ce3e0444a62999989e87c9e82cb6d4287de9
DVSneuro <david.v.smith@temple.edu>
2019-05-20 14:37:40 -0400

    initial commit

---
+++ convertAll_trust.m
@@ -0,0 +1,14 @@
+% convert all trust
+sublist = [104 105 106 107 108 109 110 112 113 111 115 116 ...
+    117 118 120 121 122 124 125 126 127 128 129 130 131 132 133 134 135 136 137 138];
+
+for s = 1:length(sublist)
+    convertTrust_BIDS(sublist(s)); % scanner participant
+    if sublist(s) == 129 || sublist(s) == 138
+        continue
+    else
+        convertTrust_BIDS(sublist(s) + 100); % friend outside
+    end
+end
+
+
