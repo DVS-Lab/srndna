22bc51cd2ae80ce5296102ac82d0f913e89d03e1
DVSneuro <david.v.smith@temple.edu>
2019-11-04 14:15:16 -0500

    change mode; no real changes, but recovering lost files

--- run_fmriprep_testing.sh
+++ run_fmriprep_testing.sh
@@ -3,18 +3,16 @@
 # usage: bash run_fmriprep.sh sub
 # example: bash run_fmriprep.sh 102
 
-sub=104
-
+sub=154
 docker run -it --rm \
 -v /data/projects/srndna/bids:/data:ro \
--v /data/projects/srndna/fmriprep2:/out \
+-v /data/projects/srndna/fmriprep:/out \
 -v /data/projects/srndna/fs_license.txt:/opt/freesurfer/fs_license.txt \
--v /data/projects/srndna/scratch2:/scratch2 \
+-v /data/projects/srndna/scratch:/scratch \
 -u $(id -u):$(id -g) \
 -w /scratch \
 poldracklab/fmriprep:1.5.1rc2 \
 /data /out \
-participant --participant_label 104 \
---use-aroma \
---fs-license-file /opt/freesurfer/fs_license.txt --cifti-output --no-submm-recon \
--w /scratch2
+participant --participant_label $sub \
+--use-aroma --fs-no-reconall --fs-license-file /opt/freesurfer/fs_license.txt \
+-w /scratch --reports-only
