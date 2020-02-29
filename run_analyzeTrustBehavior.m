3dc8483ea806c666d916508d9e8372095a2dfab7
DVSneuro <david.v.smith@temple.edu>
2020-02-28 18:11:01 -0800

    add new analyses

---
+++ run_analyzeTrustBehavior.m
@@ -0,0 +1,16 @@
+clear all; close all;
+
+% 104 105 
+sublist = [105 106 107 108 109 110 111 112 113 115 116 ...
+    117 118 120 121 122 124 125 126 127 128 129 130 131 132 133 134 135 136 137 138 140 141 142 ...
+    143 144 145 147 149:159];
+
+
+fname = sprintf('summary_task-trust_desc-postOutcomeShifts.csv');
+fid = fopen(fname,'w');
+fprintf(fid,'sub,computer_defect,computer_recip,stranger_defect,stranger_recip,friend_defect,friend_recip\n');
+for s = 1:length(sublist)
+    o = analyzeTrustBehavior(sublist(s));
+    fprintf(fid,'sub-%d,%f,%f,%f,%f,%f,%f\n',sublist(s),o.computer_defect,o.computer_recip,o.stranger_defect,o.stranger_recip,o.friend_defect,o.friend_recip');
+end
+fclose(fid);
