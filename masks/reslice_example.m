ff493573ec48d07657992ab56b0d690cd193af11
David V. Smith <david.v.smith@temple.edu>
2021-01-31 15:08:48 -0500

    add masks and reslicing code

---
+++ masks/reslice_example.m
@@ -0,0 +1,14 @@
+%-----------------------------------------------------------------------
+% Job saved on 31-Jan-2021 15:03:57 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.coreg.write.ref = {'/Users/tug87422/gitprojects/srndna/masks/ABCD_mean_func.nii,1'};
+matlabbatch{1}.spm.spatial.coreg.write.source = {
+                                                 '/Users/tug87422/gitprojects/srndna/masks/Tommy_OFC.nii,1'
+                                                 '/Users/tug87422/gitprojects/srndna/masks/Tommy_VS.nii,1'
+                                                 };
+matlabbatch{1}.spm.spatial.coreg.write.roptions.interp = 1;
+matlabbatch{1}.spm.spatial.coreg.write.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.coreg.write.roptions.mask = 0;
+matlabbatch{1}.spm.spatial.coreg.write.roptions.prefix = 'r';
